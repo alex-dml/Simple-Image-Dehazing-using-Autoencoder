4305e676737105035c8f2400799068e325afab14
Alexandra Duminil <88732189+alex-dml@users.noreply.github.com>
2022-07-08 11:51:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,89 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug  6 14:13:36 2021
+Created on Mon Jun 21 10:55:00 2021
 
-@author: duminil
+@author: alxdn
 """
-import argparse, os
-import keras
+import os, argparse, glob
+from model import build_model
+from data import get_Image_data
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+from metrics import ssim_metric, psnr_metric
 # from keras.callbacks import TensorBoard
-from data import VideoFrameGenerator
-from model import create_model
-from utils import vid_to_frame
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from skimage.transform import resize
 
-parser = argparse.ArgumentParser(description='test')
-parser.add_argument('--path_to_csv', default='F:/AE/video/nyu_CSV/*', type=str)
-parser.add_argument('--dataset', default= 'F:/nyu_HazyClear/*', type=str)
+# Argument Parser
+parser = argparse.ArgumentParser(description='training')
+
+parser.add_argument('--training_path', default='E:/Datasets/OTS_BETA2/', 
+                    type=str, help='Training dataset.')
+parser.add_argument('--valid_path', default='E:/Datasets/RESIDE/SOTS/SOTS/outdoor/', 
+                    type=str, help='validation dataset.')
+parser.add_argument('--test_path', default='E:/Datasets/RESIDE/SOTS/SOTS/indoor/', 
+                    type=str, help='test dataset.')
+
 parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
 parser.add_argument('--bs', type=int, default=4, help='Batch size')
-parser.add_argument('--epochs', type=int, default=3, help='Number of epochs')
-parser.add_argument('--gpus', type=int, default=2, help='The number of GPUs to use')
-parser.add_argument('--gpuids', type=str, default='0', help='IDs of GPUs to use')
-
+parser.add_argument('--epochs', type=int, default=20, help='Number of epochs')
 args = parser.parse_args()
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-        
-train_gen = VideoFrameGenerator(args.path_to_csv, args.dataset,
-            batch_size = args.bs,
-            nbframe = 8,
-            transform=None)
 
-# create model
-model = create_model()
 
-optimizer = Adam(lr = args.lr, amsgrad = True)
-model.compile(loss = ['mse'], optimizer = optimizer, metrics = ['accuracy'])
-model.summary()
+shape_img = (args.bs, 480, 640, 3)
 
-train_len = os.listdir(args.path_to_csv.rsplit('*', 1))
-model.fit(train_gen, steps_per_epoch = train_len//args.bs, epochs = args.epochs, 
-          shuffle = False)
+train_generator, valid_generator, test_generator = get_Image_data(args.training_path, 
+                                                      args.valid_path, 
+                                                      args.test_path,
+                                                      args.bs, 
+                                                      shape_img)
 
-model.save('Z:/AE/video/paths/test_model.h5')
+# Create the model
+model = build_model()
+# model.summary()
 
-# test video
-vid_path = 'Z:/videos_brume/fog.mp4'
-dest_folder_path = 'Z:/AE/video/data/frames/'
+my_callbacks = [
+    # tf.keras.callbacks.ModelCheckpoint(filepath='/content/drive/My Drive/h5/vid_{epoch:}.h5',
+    #                                    monitor="val_loss", save_best_only=True),
+    tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10),
+]
 
-if os.stat('Z:/AE/video/data/frames/').st_size == 0:
-    path_to_frame = vid_to_frame(vid_path, dest_folder_path)
-else:
-    path_to_frame = dest_folder_path
+optimizer = Adam(lr=args.lr)
+model.compile(optimizer=optimizer, loss = 'mse', metrics=[ssim_metric, psnr_metric])
 
-data = model.predict(path_to_frame)
+history = model.fit(train_generator, 
+                    validation_data=valid_generator, 
+                    epochs=args.epochs, 
+                    shuffle=True, 
+                    callbacks=my_callbacks)
+
+model.save('test_20_epochs.h5')
+# test 
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model train vs validation loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper right')
+plt.show()
+
+
+image = cv2.imread('E:/Datasets/sots_data/test/hazy/1400.png')/255.0
+
+image = resize(image, (480, int(480*4/3)), preserve_range=True, 
+                    mode='reflect', anti_aliasing=True )
+
+image = np.reshape(image,(1, 480, 640, 3))
+image = model.predict(image)
+
+cv2.imshow("haze", image[0,:,:,:])
+cv2.waitKey()
+
+scores = model.evaluate(test_generator)
+print("Scores [loss, ssim, psnr] :", scores)
